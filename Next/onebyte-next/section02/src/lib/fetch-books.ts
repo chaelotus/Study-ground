16a669512b5c3cd7524c83723a3b032c1e41bdca
parkchaeyeon <pcy6904@naver.com>
2025-03-26 23:01:24 +0900

    2.20 배포하기

--- Next/onebyte-next/section02/src/lib/fetch-books.ts
+++ Next/onebyte-next/section02/src/lib/fetch-books.ts
@@ -1,7 +1,7 @@
 import type { BookData } from "@/types";
 // 해당 함수는 비동기로 반환해주고 있기에 우선 Promise 객체에 제네릭으로 이전에 만들어 놓은 BookData 타입을 정의해줌
 export default async function fetchBooks(q?: string): Promise<BookData[]> {
-  let url = "http://localhost:12345/book";
+  let url = "https://onebite-books-server-main-two-flame.vercel.app/book";
 
   if (q) {
     url += `/search?q=${q}`;
