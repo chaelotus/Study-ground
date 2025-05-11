e3fcea8e10832582c98b7e066ab2480f7e567cbf
parkchaeyeon <pcy6904@naver.com>
2025-05-11 18:59:35 +0900

    5.3 풀라우트캐시 3. 동적 경로에 적용하기

--- Next/onebyte-next/section05/src/app/(with-searchbar)/search/page.tsx
+++ Next/onebyte-next/section05/src/app/(with-searchbar)/search/page.tsx
@@ -9,8 +9,14 @@ export default async function Page({
   const { q } = await searchParams; // ③
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" }
   );
+  // 브라어저에서 접속 요청 받았을 때 페이지는 계속 생성이 되겟지만
+  // 검색결과는 계속 캐싱이 이루어지기 때문에 한 번 검색이 된 결과에 대해서는 조금 더 빠르게 페이지를 응답해줄 수 있게 됨
+
+  // search page는 index page와 다르게 쿼리 스트링 같은 동적인 값에 의존하고 있기 때문에 어쩔 수 없이 static page로 설정할 수 없기 때문에
+  // 대신에 데이터 캐시를 최대한 활용하는 쪽으로 최적화를 할 수 있다.
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
 
   const books: BookData[] = await response.json();
--- Next/onebyte-next/section05/src/app/book/[id]/page.tsx
+++ Next/onebyte-next/section05/src/app/book/[id]/page.tsx
@@ -1,8 +1,38 @@
 import { BookData } from "@/types";
 import style from "./page.module.css";
+import { notFound } from "next/navigation";
 
-// test
-export default async function Page({ params }: { params: Promise<{ id: string | string[] }>}) {
+// 만약 generateStaticParams에서 return 한 데이터 제외한 값들은 not found 페이지를
+// 보여주고 싶다면 아래와 같이 dynamicParams를 false로 내보내면 됨
+// 그러면 next가 dynamicParams 값을 확인해서 지금 이 페이지 url parameter는
+// dynamic 하면 안되겠구나라고 판단하여 1,2,3 제외한 데이터는 존재하면 안되겠구나라고 해석함
+//export const dynamicParams = false;
+
+// 해당 페이지를 정적페이지로 만들고 싶다면
+// 어떤 url paramter들이 존재할 수 있는지 알려줘야 함
+// 어떠한 도서 데이터들이 빌드타임에 만들어줘야하는지
+
+// 정적인 parameter를 생성하는 함수
+// 정적인 데이터 1,2,3을 빌드타임에 만들게 된다.
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+// 해당 함수를 쓸 때 주의할 점!
+// 1. parameter data 명시할 때는 문자열로만 명시해야 함
+// 2. 해당 함수 사용하면
+// 페이지 컴포넌트 내부에 데이터 캐싱이 설정되지 않은 데이터 fetching이 존재하게 될지라도
+// 무조건 해당하는 페이지가 static page로써 강제로 설정됨.
+
+// page router에서도 위와 비슷한 함수가 있었다.
+// getStaticpaths
+// 정적으로 빌드타임에 어떠한 url parameter들이 존재할 수 있는지 설정하는 함수가 존재했다.
+// :: 동일한 역할을 한다.
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string | string[] }>;
+}) {
   const { id } = await params;
 
   //console.log(id);
@@ -11,7 +41,12 @@ export default async function Page({ params }: { params: Promise<{ id: string | 
     { cache: "no-store" }
   );
 
-  if (!response.ok) return <div>오류가 발생했습니다...</div>;
+  if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
+    return <div>오류가 발생했습니다...</div>;
+  }
 
   const book: BookData = await response.json();
 
