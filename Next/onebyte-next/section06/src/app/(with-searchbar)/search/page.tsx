e7d9c970f13205a05ddc11b4301380fe608ee3db
parkchaeyeon <pcy6904@naver.com>
2025-07-13 17:27:54 +0900

    6.3 컴포넌트 스트리밍 적용하기

--- Next/onebyte-next/section06/src/app/(with-searchbar)/search/page.tsx
+++ Next/onebyte-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,19 +1,16 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
-import {delay} from "@/util/delay"
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) {
-  const { q } = await searchParams; // ③
+async function SearchResult({ q }: { q: string }) {
+  //const { q } = await searchParams; // ③
 
-  await delay(1500)
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
-    {cache:'force-cache'}
-  ); 
+    { cache: "force-cache" }
+  );
   if (!response.ok) return <div>오류가 발생했습니다...</div>;
 
   const books: BookData[] = await response.json();
@@ -24,5 +21,20 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  return (
+    <Suspense
+      key={(await searchParams).q || ""}
+      fallback={<div>Loading...</div>}
+    >
+      <SearchResult q={(await searchParams).q || ""} />
+    </Suspense>
   );
 }
